a18fb28fa38897ef146c8f44376f500a737aba52
Yohana Avila <yohana.avila3009@gmail.com>
2023-11-12 01:08:45 -0500

    Agregando input's para función objetivo

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ function Modelo() {
     let n_variables = document.getElementById("tam_variables").value;  
     let n_restricciones = document.getElementById("tam_restricciones").value;   
 
-    document.getElementById("modelo_metodo").innerHTML = '<h4><em> Función </em>Objetivo</h4>';
+    document.getElementById("modelo_metodo").innerHTML = '';
     
     for (let i = 0; i < n_variables; i++) {
 
