375e85a2f7dc352abc168bdfa155a3202702f5be
Yohana Avila <yohana.avila3009@gmail.com>
2023-11-12 00:46:47 -0500

    Iniciando codificación del modelo

---
+++ js/index.js
@@ -0,0 +1,24 @@
+
+
+function Limpiar() {
+    document.getElementById("tam_variables").value = '';  
+    document.getElementById("tam_restricciones").value = '';   
+}
+
+
+function Modelo() {
+    let n_variables = document.getElementById("tam_variables").value;  
+    let n_restricciones = document.getElementById("tam_restricciones").value;   
+
+    document.getElementById("modelo_metodo").innerHTML = '<h4><em> Función </em>Objetivo</h4>';
+    
+    for (let i = 0; i < n_variables; i++) {
+
+        document.getElementById("modelo_metodo").innerHTML +='';
+    }
+
+    for (let i = 0; i < tama; i++) {
+        document.getElementById("input_esp").innerHTML += '<br><input type="text" class="form-control" name="txt_especificacion[]" placeholder="Escriba la especificación '+ (i+1) +'" required>';  
+    }
+
+}
