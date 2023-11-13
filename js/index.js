8f725464c72ad74498ebc6f941c859e9a096a05d
Yohana Avila <yohana.avila3009@gmail.com>
2023-11-12 19:12:41 -0500

    Agregando los elementos para la captura de datos(matriz) para el calculo del método

--- js/index.js
+++ js/index.js
@@ -1,24 +1,64 @@
-
-
-function Limpiar() {
-    document.getElementById("tam_variables").value = '';  
-    document.getElementById("tam_restricciones").value = '';   
+function Editar(){
+    document.getElementById("captura_cantidad").style = "display: block";
+    document.getElementById("captura_cantidad_1").style = "display: block";
+    document.getElementById("modelo_funcion").style = "display: none";
+    document.getElementById("modelo_restricciones").style = "display: none";
+    document.getElementById("objetivo").style = "display: none";   
 }
-
 
 function Modelo() {
     let n_variables = document.getElementById("tam_variables").value;  
     let n_restricciones = document.getElementById("tam_restricciones").value;   
 
-    document.getElementById("modelo_metodo").innerHTML = '';
+    // let funcion = new Array();
     
-    for (let i = 0; i < n_variables; i++) {
+    if (n_variables <= 20 && n_variables >= 2) {
 
-        document.getElementById("modelo_metodo").innerHTML +='';
-    }
+        // Mostrando el Modelo
+        document.getElementById("captura_cantidad").style = "display: none";
+        document.getElementById("captura_cantidad_1").style = "display: none";
+        document.getElementById("modelo_funcion").style = "display: block";
+        document.getElementById("modelo_restricciones").style = "display: block";
+        document.getElementById("objetivo").style = "display: block";
+        
+        // Reiniciando el modelo
+        document.getElementById("N_restriccion").innerHTML ='';
+        document.getElementById("funcion").innerHTML ='';
+        
+        // Agregando la función
+        document.getElementById("funcion").innerHTML +='<div class="container"> <br></div> <label><h5>Función: &nbsp&nbsp&nbsp</h5></label><br>';
+        for (let i = 0; i < n_variables-1; i++) {
+            document.getElementById("funcion").innerHTML +='<center> <input  id="num_X'+(i+1)+'" type="number" style="width : 100px; border: 5px solid rgba(0, 0, 0, 0.0);" class="form-control">';
+            document.getElementById("funcion").innerHTML +='<div><span id="var_X'+(i+1)+'" style="font-size: 1.5em; color: azure;">&nbspX<sub id="variable">'+(i+1)+'</sub><span id="signo">+</span>&nbsp</span></div><center> '; 
+        }
+        document.getElementById("funcion").innerHTML +='<center> <input  id="num_X'+n_variables+'" type="number" style="width : 100px; border: 5px solid rgba(0, 0, 0, 0.0);" class="form-control">';
+        document.getElementById("funcion").innerHTML +='<div><span id="var_X'+n_variables+'" style="font-size: 1.5em; color: azure;">&nbspX<sub id="variable">'+n_variables+'</sub></span></div><center> '; 
+        
+        // Agregando las Restricciones 
+        for (let i = 0; i < n_restricciones; i++) {
+            document.getElementById("N_restriccion").innerHTML +='<div class="container"> <br></div> <label><h5>Restriccion N° '+(i+1)+': &nbsp&nbsp&nbsp</h5></label>';
 
-    for (let i = 0; i < tama; i++) {
-        document.getElementById("input_esp").innerHTML += '<br><input type="text" class="form-control" name="txt_especificacion[]" placeholder="Escriba la especificación '+ (i+1) +'" required>';  
+            for (let y = 0; y < n_variables; y++) {
+                
+                if (y < n_variables-1) {
+                    document.getElementById("N_restriccion").innerHTML += '<center> <input  id="num_X'+(y+1)+'_Y'+(i+1)+'" type="number" style="width : 90px; border: 4px solid rgba(0, 0, 0, 0.0);" class="matriz form-control"> </center>';
+                    document.getElementById("N_restriccion").innerHTML += '<center> <div><span id="var_X'+(y+1)+'_Y'+(i+1)+'" style="font-size: 1.5em; color: azure;">&nbspX<sub id="variable">'+(y+1)+'</sub><span id="signo">+</span>&nbsp</span></div> </center>';
+                }else{
+                    document.getElementById("N_restriccion").innerHTML += '<center> <input  id="num_X'+(y+1)+'_Y'+(i+1)+'" type="number" style="width : 90px; border: 4px solid rgba(0, 0, 0, 0.0);" class="matriz form-control"> </center>';
+                    document.getElementById("N_restriccion").innerHTML += '<center> <div><span id="var_X'+(y+1)+'_Y'+(i+1)+'" style="font-size: 1.5em; color: azure;">&nbspX<sub id="variable">'+(y+1)+'</sub>&nbsp</span></div> </center>';
+                }
+            } 
+            document.getElementById("N_restriccion").innerHTML +='&nbsp&nbsp&nbsp<select style="width : 90px; border: 4px solid rgba(0, 0, 0, 0.0);" class="matriz form-select-sm"><option value="1"> ≤ </option> <option value="2"> ≥ </option> <option value="3"> = </option> </select> &nbsp&nbsp&nbsp';
+            document.getElementById("N_restriccion").innerHTML += '<center> <input  id="num_X'+(i+1)+'" type="number" style="width : 100px; border: 5px solid rgba(0, 0, 0, 0.0);" class="form-control"> </center>';
+        }
+
+    }else{
+        alert("El número de variables debe ser mayor a 02 y menor a 20, y el número de restricciones debe ser Mayor a 01 Menor o igual a 20");
     }
 
 }
+
+
+void function test() {
+    console.log('test function executed');
+};
