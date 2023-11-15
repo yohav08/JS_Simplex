9193c6f553e960cb3d903851e672eb7e03d7acd0
Yohana Avila <yohana.avila3009@gmail.com>
2023-11-14 23:50:57 -0500

    Agregando las condiciones para las variables artificiales, de holgura y exceso

--- js/index.js
+++ js/index.js
@@ -48,25 +48,122 @@ function Modelo() {
                     document.getElementById("N_restriccion").innerHTML += '<center> <div><span id="var_X'+(y+1)+'_Y'+(i+1)+'" style="font-size: 1.5em; color: azure;">&nbspX<sub id="variable">'+(y+1)+'</sub>&nbsp</span></div> </center>';
                 }
             } 
-            document.getElementById("N_restriccion").innerHTML +='&nbsp&nbsp&nbsp<select style="width : 90px; border: 4px solid rgba(0, 0, 0, 0.0);" class="matriz form-select-sm"><option value="1"> ≤ </option> <option value="2"> ≥ </option> <option value="3"> = </option> </select> &nbsp&nbsp&nbsp';
+            document.getElementById("N_restriccion").innerHTML +='&nbsp&nbsp&nbsp<select id="variables_adicionales'+(i+1)+'" style="width : 90px; border: 4px solid rgba(0, 0, 0, 0.0);" class="matriz form-select-sm"><option value="menor_igual"> ≤ </option> <option value="mayor_igual"> ≥ </option> <option value="igual"> = </option> </select> &nbsp&nbsp&nbsp';
             document.getElementById("N_restriccion").innerHTML +='<center> <input id="num_X'+(i+1)+'" type="number" style="width : 100px; border: 5px solid rgba(0, 0, 0, 0.0);" class="matriz form-control"> </center>';
         }
     }else{
         alert("El número de variables debe ser mayor a 02 y menor a 20, y el número de restricciones debe ser Mayor a 01 Menor o igual a 20");
     }
-
 }
 
 function Generar_metodo(){
+
     //filas
     let n_variables = document.getElementById("tam_variables").value;  
     //columnas
-    let n_restricciones = document.getElementById("tam_restricciones").value; 
-    
- 
+    let n_restricciones = document.getElementById("tam_restricciones").value;
+
+    let variable_r = 0;
+    let variable_s = 0;
+    let variable_h = 0;
+
     let matriz = new Array(2);
     matriz[0] = new Array(n_variables+1);
     matriz[1] = new Array(n_restricciones);
+
+
+    // Reiniciando las variables agregadas
+    document.getElementById("modelo_textual_1").innerHTML ='';
+    let posicion_artificial = 0;
+    
+    for (let i = 0; i < n_restricciones; i++) {
+        let combo_1 = document.getElementById("variables_adicionales"+(i+1));
+        let rest_1 = combo_1.options[combo_1.selectedIndex].value;
+
+        // console.log("HOLA: "+i+": "+rest_1);
+
+        
+        if (rest_1 == "mayor_igual") {
+            variable_s++;
+            variable_r++;
+            
+            
+            document.getElementById("modelo_textual_1").innerHTML +='<li style="font-size: 1.2rem; color: aliceblue;"><span>'+'&nbsp&nbsp&nbsp '+(i+1)+'. Tiene signo "≥" (mayor igual) por lo que se restará la variable de exceso S'+variable_s+' y se sumará la variable artificial R'+variable_r+'.</span></li>';
+    
+        } else if (rest_1 == "menor_igual") {
+            variable_h++;
+            document.getElementById("modelo_textual_1").innerHTML +='<li style="font-size: 1.2rem; color: aliceblue;"><span>'+'&nbsp&nbsp&nbsp '+(i+1)+'. Tiene signo "≤" (menor igual) por lo que se agregará la variable de holgura H'+(variable_h)+'.</span></li>';
+            
+        } else if (rest_1 == "igual") {
+            variable_r++;
+            document.getElementById("modelo_textual_1").innerHTML +='<li style="font-size: 1.2rem; color: aliceblue;"><span>'+'&nbsp&nbsp&nbsp '+(i+1)+'. Tiene signo "=" (igual) por lo que se agregará la variable artificial R'+variable_r+'.</span></li>';
+            
+        }
+    }
+    
+    let variable_aux= variable_r+ variable_s+variable_h;
+    console.log("Variables artificiales: "+variable_aux);
+
+    let matriz_aux = new Array(2);
+    matriz_aux[0] = new Array(variable_aux);
+    matriz_aux[1] = new Array(n_restricciones);
+
+    
+    for (let i = 0; i < variable_aux; i++) {
+        matriz_aux[0][i]=0;
+        matriz_aux[1][i]=0;
+
+        // for (let j = 0; j < n_restricciones; j++) {
+        //     matriz_aux[i][j] = 0;  
+        // }
+        
+    }
+
+    let auxiliar=0;
+    for (let i = 0; i < n_restricciones; i++) {
+        
+
+        let combo_1 = document.getElementById("variables_adicionales"+(i+1));
+        let rest_1 = combo_1.options[combo_1.selectedIndex].value;
+
+        if (rest_1 == "mayor_igual") {
+            // variable_s++; // -1
+            //  variable_r++; // 1
+            console.log("Mayor igual: pos i-"+i+" pos y:"+auxiliar+" valor: "+(-1));
+            matriz_aux [i][auxiliar] = -1;
+            auxiliar++;
+
+            //auxiliar general que es utilizado para cada var artificial, crear un auxiliar extra para
+            // el problema de la variable R, que se aregue a la matriz 
+            
+            
+        }if (rest_1 == "menor_igual") {
+            // variable_h++;
+            console.log("Menor igual: pos i-"+i+" pos y:"+auxiliar+" valor: "+1);
+            matriz_aux [i][auxiliar] = 1;
+            auxiliar++;
+
+        }else if (rest_1 == "igual") {
+            // variable_r++; // 1            
+            console.log("igual: pos i-"+i+" pos y:"+auxiliar+" valor: "+1);
+            matriz_aux [i][auxiliar] = 1;
+            auxiliar++;
+        }
+        
+        
+        //mensajito de las restricciones
+
+
+    }
+    console.log(matriz_aux);
+
+
+
+    
+     
+    
+ 
+    
 
     let aux = n_variables-1;
 
@@ -76,7 +173,20 @@ function Generar_metodo(){
         }   
         matriz[i][n_restricciones] = document.getElementById("num_X"+(i+1)+"").value;     
     }
-    console.log(matriz)
+
+    // let combo = document.getElementById("objetivo_1");
+    // let objetivo = combo.options[combo.selectedIndex].text;
+
+    // if ( objetivo == "Maximizar") {
+    //     console.log('Maximización');
+        
+    // }else if (objetivo == "min") {
+    //     console.log('Minimizar');
+    // }
+
+    console.log("Matriz:")
+    console.log(matriz);
+    
 }
 
 void function test() {
